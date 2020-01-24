f71a771cbd5c1df6d046c162b5899ebb517cf1f8
Default user <yvancharpak@gmail.com>
2020-01-24 15:54:52 +0000

    casi casi pero se arreglo

--- Ejercicio_1/HernandezYvan_Ejercicio02.py
+++ Ejercicio_1/HernandezYvan_Ejercicio02.py
@@ -1,53 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+class PolyFit:
+    def __init__(self, degree=0):
+        self.degree = degree
+        self.betas = np.zeros(degree)
+    
+    def y(self,x,w,m):
+        y = np.zeros(len(x))
+        for i in range(m):
+            y += w[i]*x**i
+        return y
+
+    def err(self,y,t):
+        err=0
+        for i in range(len(y)):
+            err += (y[i]-t[i])**2
+        err = 1/2*err
+        return err  
+
+    def fit(self,X,Y):
+        M = self.degree
+        N = len(X)
+        fig1 = plt.figure(figsize=(12,12))
+        #Construir el sistema
+        S = np.zeros(shape=(N,M))
+        for j in range(M):
+            S[:,j] = X[:]**(j)
+        #Resolver el sistema
+        S_1 = np.linalg.pinv(S)
+        w = np.dot(S_1,Y)
+        #Dar el beta
+        self.betas = w
+
+    def predict(self,X):
+        w = self.betas
+        m = self.degree
+        LaPrediccion = self.y(X,w,m)
+        return LaPrediccion
+
+    def score(self,x_1, x_2):
+        #Guardar el RMS
+        RMS = np.zeros(self.degree())
+        RMS = np.sqrt(2*self.err(y(x_1,w,i),x_2)/len(x_1))
+        return RMS
+
+    def graficarRMS(self,RMS):
+        #Graficar el error
+        fig2 =plt.figure(figsize=(12,12))
+        plt.plot(range(len(RMS)),RMS, c='b')
+        plt.savefig("LaSegunda.png", bbox_inches='tight')
+
+    def graficarFit(self,X,Y):
+        #Graficar los fits
+        M = self.degree
+        x = np.linspace(np.min(X),np.max(X),100)
+        y = self.predict(x)
+        plt.title('M='+ str(M))
+        plt.plot(x,y,'r')
+        plt.scatter(X,Y,s=80,marker='o', facecolors='none',edgecolor='b')
+        plt.savefig("LaPrimera.png", bbox_inches='tight')   
+
+        
+                
 datos = np.genfromtxt('numeros_20.txt')
 DatosEntreno = datos[0:10]
-DatosTest = datos[11:20]
-
-def y(x,w,m):
-    y = np.zeros(len(x))
-    for i in range(m):
-        y += w[i]*x**i
-    return y
-    
-def err(y,t):
-    err=0
-    for i in range(len(y)):
-        err += (y[i]-t[i])**2
-    err = 1/2*err
-    return err  
-
-def solver(M):
-    
-    TraErr = np.zeros(M)
-    TesErr = np.zeros(M)
-    
-    fig1 = plt.figure(figsize=(12,12))
-    for k in range(M):
-        i = k+1
-        #Construir el sistema
-        S = np.zeros(shape=(i,i))
-        for j in range(i):
-            S[:i,j] = DatosEntreno[:i,0]**(j)
-        #Resolver el sistema
-        S_1 = np.linalg.inv(S)
-        w = np.dot(S_1,DatosEntreno[:i,1])
-        #Guardar el RMS
-        TraErr[k] = err(y(DatosEntreno[:,0],w,i),DatosEntreno[:,1])
-        TesErr[k] = err(y(DatosTest[:,0],w,i),DatosTest[:,1])
-        
-        #Graficar los fits
-        x = np.linspace(np.min(DatosEntreno[:,0]),np.max(DatosEntreno[:,0]),100)
-        plt.subplot(M/2,M/2,i)
-        plt.title('M='+ str(k))
-        plt.plot(x,y(x,w,i),'r')
-        plt.scatter(DatosEntreno[:,0],DatosEntreno[:,1],s=80,marker='o', facecolors='none',edgecolor='b')
-    plt.savefig("LaPrimera.png", bbox_inches='tight')   
-    #Graficar el error
-    fig2 =plt.figure(figsize=(12,12))
-    plt.plot(range(M),TraErr, c='b')
-    plt.plot(range(M),TesErr, c='r')
-    plt.savefig("LaSegunda.png", bbox_inches='tight')
-        
-solver(4)
+X = DatosEntreno[:,0]
+Y = DatosEntreno[:,1]
+Entreno = PolyFit(3)
+Entreno.fit(X,Y)
+Entreno.graficarFit(X,Y)
